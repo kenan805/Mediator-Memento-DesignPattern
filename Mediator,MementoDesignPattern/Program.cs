3b7b52262cdafae501f3e9d5ff24fb12615c5871
Kənan İdayatov <idayatovkenan7@gmail.com>
2021-10-19 16:13:29 +0400

    New commit

---
+++ Mediator,MementoDesignPattern/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator_MementoDesignPattern
+{
+    public interface IChatMediator
+    {
+        void AddUser(User user);
+        void SendMessageToAllUsers(string message, User currentUsr);
+    }
+
+    public class ChatMediator : IChatMediator
+    {
+        private List<User> users;
+
+        public ChatMediator()
+        {
+            users = new List<User>();
+        }
+
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
+        public void SendMessageToAllUsers(string message, User currentUsr)
+        {
+            users.ForEach(w =>
+            {
+                if (w != currentUsr)    // current userden basqa butun userlere mesaj gedir
+                {
+                    w.ReceiveMessage(message);
+                }
+            });
+        }
+    }
+
+    public abstract class User
+    {
+        private IChatMediator chatMediator;
+        private string name;
+
+        public User(IChatMediator chatMediator, string name)
+        {
+            this.chatMediator = chatMediator;
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public IChatMediator ChatMediator
+        {
+            get
+            {
+                return chatMediator;
+            }
+        }
+
+        public abstract void SendMessage(string message);
+        public abstract void ReceiveMessage(string message);
+    }
+
+    public class BasicUser : User
+    {
+        public BasicUser(IChatMediator chatMediator, string name)
+            : base(chatMediator, name)
+        {
+        }
+
+        public override void SendMessage(string message)
+        {
+            this.ChatMediator.SendMessageToAllUsers(message, this);
+        }
+
+        public override void ReceiveMessage(string message)
+        {
+            Console.WriteLine("Basic User: " + this.Name + " receive message: " + message);
+        }
+    }
+
+    public class PremiumUser : User
+    {
+        public PremiumUser(IChatMediator chatMediator, string name)
+            : base(chatMediator, name)
+        {
+        }
+
+        public override void SendMessage(string message)
+        {
+            this.ChatMediator.SendMessageToAllUsers(message, this);
+        }
+
+        public override void ReceiveMessage(string message)
+        {
+            Console.WriteLine("Premium User: " + this.Name + " receive message: " + message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IChatMediator chatMediator = new ChatMediator();
+            //3 online var chatda
+            User user1 = new BasicUser(chatMediator, "User 1");
+            User user2 = new PremiumUser(chatMediator, "User 2");
+            User user3 = new PremiumUser(chatMediator, "User 3");
+            chatMediator.AddUser(user1);
+            chatMediator.AddUser(user2);
+            chatMediator.AddUser(user3);
+
+            //yenisi add olundu
+            User newUser = new BasicUser(chatMediator, "New User");
+            chatMediator.AddUser(newUser);
+
+            //bu haqda melumat digerlerine gedir
+            newUser.SendMessage("New user is online.");
+        }
+    }
+}
---
+++ MementoDesignPattern/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace MementoDesignPattern
+{
+    public class Calculator
+    {
+        private int result = 0;
+
+        public int Add(int a)
+        {
+            this.result += a;
+            return this.result;
+        }
+
+        public int Subtract(int a)
+        {
+            this.result -= a;
+            return this.result;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Result: " + this.result);
+        }
+
+        public CalculatorMemento CreateMemento()
+        {
+            CalculatorMemento calcMemento = new CalculatorMemento();
+            calcMemento.SetState(this.result);
+            return calcMemento;
+        }
+
+        public void SetMemento(CalculatorMemento memento)
+        {
+            this.result = memento.GetState();
+        }
+    }
+
+    public class CalculatorMemento
+    {
+        private int state;
+
+        public int GetState()
+        {
+            return this.state;
+        }
+
+        public void SetState(int state)
+        {
+            this.state = state;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Add(4);        // result 4
+            calc.Add(3);        // result 7
+            calc.Subtract(1);   // result 6
+
+            CalculatorMemento checkPoint1 = calc.CreateMemento();
+            calc.Add(1);        // result 7
+            calc.Print();       // print 7
+
+            calc.SetMemento(checkPoint1);
+            calc.Print();       // result 6
+        }
+    }
+}
